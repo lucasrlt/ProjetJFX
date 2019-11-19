af11f5b4733a656adc6a595a4222a7e97878819d
Cynix69 <hadguy@free.fr>
2019-11-19 15:15:35 +0100

    Rajout des fonctions du Modele.

--- src/projet/Modele.java
+++ src/projet/Modele.java
@@ -4,10 +4,37 @@ import java.util.Observable;
 
 public class Modele extends Observable {
     Grille grille;
+    int lastC, lastR;
 
     public Modele() {
         super();
 
         grille = new Grille(3, 3);
     }
-}
+        public void enfoncerClicGrille(int c, int r) {
+            // TODO
+            System.out.println("startDD : " + c + "-" + r);
+            setChanged();
+            notifyObservers();
+        }
+
+        public void relacherCliclGrille(int c, int r) {
+            // TODO
+
+            // mémoriser le dernier objet renvoyé par parcoursDD pour connaitre la case de relachement
+
+            System.out.println("stopDD : " + c + "-" + r + " -> " + lastC + "-" + lastR);
+            setChanged();
+            notifyObservers();
+        }
+
+        public void parcoursGrille(int c, int r) {
+            lastC = c;
+            lastR = r;
+            System.out.println("parcoursDD : " + c + "-" + r);
+            setChanged();
+            notifyObservers();
+        }
+
+
+    }
